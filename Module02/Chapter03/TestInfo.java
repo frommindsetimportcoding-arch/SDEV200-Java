be73856dbce85212e8e84b8244aba021e3908d2b
Kismet <frommindsetimportcoding@gmail.com>
2026-09-12 23:04:55 -0400

    Adding You Do It Exercises for Chapter 03 Tutorial Assignment

---
+++ Module02/Chapter03/TestInfo.java
@@ -0,0 +1,11 @@
+// Nathan Thomas
+// p. 89
+
+public class TestInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Calling method from another class:");
+        ParadiseInfo.displayInfo();
+    }
+}
